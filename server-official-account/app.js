18e7338481c8adc58ebd162f04d087ebc656f542
thu <mingfei.net@gmail.com>
2018-02-23 15:56:45 +0800

    Update

--- server-official-account/app.js
+++ server-official-account/app.js
@@ -1,5 +1,19 @@
 const EXPRESS = require('express');
+const WEICHAT = require('wechat');
+
+let config = {
+    appid: 'wxd29115390c39d130',
+    token: 'weixin',
+    encodingAESKey: 'cbqIt86C0vd14ArjfOSDionCsPuTCMB702D23XYW13U',
+};
 
 let app = new EXPRESS();
+
+app.use(EXPRESS.query());
+
+app.get('/', WEICHAT(config, (req, res, next) => {
+    let message = req.weixin;
+    console.log(message);
+}));
 
 app.listen(3000);
